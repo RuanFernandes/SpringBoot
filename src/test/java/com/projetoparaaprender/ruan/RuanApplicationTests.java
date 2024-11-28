7ea46128c7a0147fe4d986e7929b2747bba800d5
Ruan Fernandes Guimarães <ruan_guimaraes2004@hotmail.com>
2024-11-28 01:35:22 -0300

    first

---
+++ src/test/java/com/projetoparaaprender/ruan/RuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoparaaprender.ruan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
